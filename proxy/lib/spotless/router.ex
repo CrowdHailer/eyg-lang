e5b21698c6c2cde624f39930853031840c4f6469
Peter <peterhsaxton@gmail.com>
2021-06-20 16:15:40 +0100

    handle response

--- proxy/lib/spotless/router.ex
+++ proxy/lib/spotless/router.ex
@@ -11,7 +11,7 @@ defmodule Spotless.Router do
     :yes = :global.re_register_name({Spotless.Client, client_id}, self())
 
     receive do
-      {:request, forwarded} ->
+      {:request, forwarded, response_id} ->
         %Raxx.Request{method: method, raw_path: path, query: query, headers: headers, body: body} =
           forwarded
 
@@ -23,7 +23,8 @@ defmodule Spotless.Router do
             raw_path: path,
             query: query,
             headers: Enum.into(headers, %{}),
-            body: body
+            body: body,
+            response_id: response_id
           })
         )
     after
@@ -32,15 +33,35 @@ defmodule Spotless.Router do
     end
   end
 
+  def handle_request(%Raxx.Request{path: ["response", response_id], body: body}, _) do
+      case :global.whereis_name({Spotless.Response, client_id}) do
+          pid when is_pid(pid) ->
+            send(pid, {:response, body})
+
+            Raxx.response(:ok)
+            |> Raxx.set_body("I've replied")
+
+          :undefined ->
+            Raxx.response(:ok)
+            |> Raxx.set_body("no pid found")
+        end
+  end
+
   def handle_request(request, _) do
     case String.split(Raxx.request_host(request), ".") do
       [client_id, "spotless", "run"] when client_id != "api" ->
         case :global.whereis_name({Spotless.Client, client_id}) do
           pid when is_pid(pid) ->
-            send(pid, {:request, request})
+            response_id = response_id()
+            :yes = :global.re_register_name({Spotless.Response, response_id}, self())
+            send(pid, {:request, request, response_id})
 
+            receive do
+                {:response, body} ->
             Raxx.response(:ok)
-            |> Raxx.set_body("I've forwarded it")
+            |> Raxx.set_body(body)
+            end
+            
 
           :undefined ->
             Raxx.response(:ok)
@@ -71,5 +92,10 @@ defmodule Spotless.Router do
   def client_id do
     <<id::binary-6, _::binary>> = Base.encode32(:crypto.strong_rand_bytes(5))
     id
+  end
+
+
+  def response_id do
+    Base.encode32(:crypto.strong_rand_bytes(5))
   end
 end
