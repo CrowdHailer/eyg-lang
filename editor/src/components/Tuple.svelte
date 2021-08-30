0c08bfcdb4b38b68674e300cc0b0f6a06f4b6e68
Peter <peterhsaxton@gmail.com>
2021-08-30 20:23:56 +0100

    render tuple

---
+++ editor/src/components/Tuple.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Expression from "./Expression.svelte";
+  import Indent from "./Indent.svelte";
+  export let elements;
+  export let update_tree;
+</script>
+
+[{#each elements.toArray() as element, i}
+  <Expression
+    expression={element}
+    {update_tree}
+    on:pinpoint
+    on:depoint
+  />,&nbsp;
+{/each}]
