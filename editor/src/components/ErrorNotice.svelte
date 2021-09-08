046766e7bbb18791b428b429020aab4b9e463e27
Peter <peterhsaxton@gmail.com>
2021-09-08 10:02:31 +0100

    error notice component

---
+++ editor/src/components/ErrorNotice.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let type_;
+</script>
+
+{#if type_.type == "Error"}
+  <div class=" bg-red-100 border-t-2 border-red-300 py-1 px-4">
+    {JSON.stringify(type_[0])}
+  </div>
+{/if}
