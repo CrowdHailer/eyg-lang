4571d38bfd0c2c1c06885d9209be41b16c66ab9d
Peter Saxton <peterhsaxton@gmail.com>
2021-11-27 11:39:53 +0100

    render error on variable

--- editor/src/components/Variable.svelte
+++ editor/src/components/Variable.svelte
@@ -1,14 +1,19 @@
 <script>
+  import * as Typer from "../gen/eyg/typer";
 
   export let position;
   export let metadata;
   export let label;
   export let global;
+
+  let error = false
+  $: error = Typer.is_error(metadata)
 </script>
 
 <!-- Don't edit delte replace with autocomplete -->
 <span
-  class="outline-none text-blue-500 border-2 border-indigo-300 border-opacity-0 focus:border-opacity-100 outline-none rounded"
+  class="outline-none text-blue-500 border-2 border-white focus:border-indigo-300 outline-none rounded"
+  class:border-red-500={error}
   tabindex="-1"
   data-position={"p" + position.join(",")}>{label}</span
 >
