6c7dab0b302c1e47171ed0a26261737ce364b8e7
Peter <peterhsaxton@gmail.com>
2022-05-21 10:06:40 +0200

    remove test leftovers

--- editor/src/workspace/Static.svelte
+++ editor/src/workspace/Static.svelte
@@ -7,9 +7,4 @@
   <p class="bg-gray-200 p-1">
     <span>Output = </span><span>{value}</span>
   </p>
-  <span
-    class="inline-block w-36"
-    class:bg-green-500={value == '"True"'}
-    class:bg-red-500={!value == '"True"'}>Test</span
-  >
 </div>
