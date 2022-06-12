23c068c5d0602def7572ffeda6292f693fbe6186
Peter <peterhsaxton@gmail.com>
2022-06-12 13:27:59 +0200

    better app first setup

--- editor/src/workspace/UI.svelte
+++ editor/src/workspace/UI.svelte
@@ -2,6 +2,6 @@
   export let rendered;
 </script>
 
-<div class="w-full h-full bg-gray-100 p6" data-ui="my-app">
+<div class="w-full h-full bg-gray-100 flex" data-ui="my-app">
   {@html rendered}
 </div>
