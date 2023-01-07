4294e7a1e0c22cd1ff4f3c510cffd26ebe586858
Peter <peterhsaxton@gmail.com>
2023-01-07 15:20:55 +0100

    F keys should act as normal

--- eyg/src/browser_ffi.js
+++ eyg/src/browser_ffi.js
@@ -1,9 +1,16 @@
 // for atelier_ffi must be top level
+const fKey = new RegExp("^F\\d+$");
+
 export function listenKeypress(dispatch) {
   // https://medium.com/analytics-vidhya/implementing-keyboard-controls-or-shortcuts-in-javascript-82e11fccbf0c
   document.addEventListener("keydown", function (event) {
     if (document.activeElement === document.body) {
-      if (event.altKey || event.ctrlKey || event.metaKey) {
+      if (
+        event.altKey ||
+        event.ctrlKey ||
+        event.metaKey ||
+        fKey.test(event.key)
+      ) {
         // These should behave normally
         return;
       } else {
